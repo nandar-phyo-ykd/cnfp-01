959e4742ceb03dacb68874d5512b7bbac5bcaa73
nandar-phyo-ykd <94099438+nandar-phyo-ykd@users.noreply.github.com>
2024-09-14 12:21:09 +0900

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,11 @@
+terraform {
+  cloud {
+    organization = "hellocloud-mentorship"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
 
+    workspaces {
+      name = "random_string_proj"
+    }
+  }
+}
